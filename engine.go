4c8ae4a9d04ba9461cb36112358e8ff9b2f5aaba
Rhuan Chaves <rhuan.chaves@vollsolutions.com.br>
2023-12-19 22:46:30 -0300

    refactor on tests

--- engine.go
+++ engine.go
@@ -9,7 +9,11 @@ type Action struct {
 
 func ProcessWorld(world *box2d.B2World, playerStore map[string]*box2d.B2Body, action Action) error{
 	player := playerStore[action.player]
-	player.ApplyForceToCenter(box2d.B2Vec2{X: 0, Y: 10.0}, true)
+	if(action.action == "UP") {
+		player.ApplyForceToCenter(box2d.B2Vec2{X: 0.0, Y: 10.0}, true)
+	} else {
+		player.ApplyForceToCenter(box2d.B2Vec2{X: -10.0, Y: 0.0}, true)
+	}
 	
 	for i := 0; i < 60; i++ {
 		world.Step(1.0 / 60.0, 6, 2)
--- engine_test.go
+++ engine_test.go
@@ -21,26 +21,30 @@ func almostEqual(a, b float64) bool{
     return (d / math.Abs(b)) < tolerance
 }
 
+func setupWorld() (world box2d.B2World, character *box2d.B2Body) {
+	//Setup world
+	gravity := box2d.MakeB2Vec2(0, 0)
+	world = box2d.MakeB2World(gravity)
+	
+	//Setup Character
+	bd := box2d.MakeB2BodyDef()
+	bd.Position.Set(2.0, 2.0)
+	bd.Type = box2d.B2BodyType.B2_dynamicBody
+	bd.FixedRotation = false	
+	bd.AllowSleep = false
+	character = world.CreateBody(&bd)
+	shape := box2d.MakeB2PolygonShape()
+	shape.SetAsBox(0.20, 0.20)
+	fd := box2d.MakeB2FixtureDef()
+	fd.Shape = &shape
+	fd.Density = 1.0
+	character.CreateFixtureFromDef(&fd)
+	return
+}
+
 func TestActions(t *testing.T) {
 	t.Run("player goes up on action UP", func(t *testing.T) {
-		//Setup world
-		gravity := box2d.MakeB2Vec2(0, 0)
-		world := box2d.MakeB2World(gravity)
-		
-		//Setup Character
-		bd := box2d.MakeB2BodyDef()
-		bd.Position.Set(2.0, 2.0)
-		bd.Type = box2d.B2BodyType.B2_dynamicBody
-		bd.FixedRotation = false	
-		bd.AllowSleep = false
-		character := world.CreateBody(&bd)
-		shape := box2d.MakeB2PolygonShape()
-		shape.SetAsBox(0.20, 0.20)
-		fd := box2d.MakeB2FixtureDef()
-		fd.Shape = &shape
-		fd.Density = 1.0
-		character.CreateFixtureFromDef(&fd)
-
+		world, character := setupWorld()
 		playerName :=  "John"
 		action := Action{action: "UP", player: playerName}
 		playerStore := make(map[string] *box2d.B2Body)
@@ -60,6 +64,28 @@ func TestActions(t *testing.T) {
 			t.Errorf("player '%s' didn't go up correctly. Got %.1f expected %.1f", playerName, character.GetPosition().Y, 3.0)
 		}
 
+	})
 
+	t.Run("player goes left on action LEFT", func(t *testing.T) {
+		world, character := setupWorld()
+		playerName :=  "John"
+		action := Action{action: "LEFT", player: playerName}
+		playerStore := make(map[string] *box2d.B2Body)
+		playerStore[playerName] = character
+		//Acions to perform
+		err := ProcessWorld(&world, playerStore, action)
+
+		if err != nil {
+			t.Fatalf("Error processing world, '%v'", action)
+		}
+
+		if !almostEqual(character.GetPosition().X, 1.0) {
+			t.Errorf("player '%s' didn't go left correctly. Got %.1f expected %.1f", playerName, character.GetPosition().X, 1.0)
+		}
+
+		if !almostEqual(character.GetPosition().Y, 2.0) {
+			t.Errorf("player '%s' moved vertically while going left", playerName)
+		}
+		
 	})
 }
