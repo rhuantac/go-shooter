e079e7795bb488741b28b3f76e47c2e97b05943e
Rhuan Chaves <rhuan.chaves@vollsolutions.com.br>
2023-12-21 21:54:19 -0300

    Refactor engine

--- engine.go
+++ engine.go
@@ -12,28 +12,57 @@ const (
 	MoveDown InputType = "DOWN"
 )
 
-const MoveSpeed = 10.0
+const moveSpeed = 10.0
 type Action struct {
 	input InputType
 	player string
 }
 
+type WorldProcessor struct {
+	World *box2d.B2World
+}
 
-
-func ProcessWorld(world *box2d.B2World, playerStore map[string]*box2d.B2Body, action Action) error{
+func (e WorldProcessor) Process(playerStore map[string]*box2d.B2Body, action Action) error{
 	player := playerStore[action.player]
 	if action.input == MoveUp {
-		player.ApplyForceToCenter(box2d.B2Vec2{X: 0.0, Y: MoveSpeed}, true)
+		player.ApplyForceToCenter(box2d.B2Vec2{X: 0.0, Y: moveSpeed}, true)
 	} else if action.input == MoveLeft {
-		player.ApplyForceToCenter(box2d.B2Vec2{X: MoveSpeed * -1, Y: 0.0}, true)
+		player.ApplyForceToCenter(box2d.B2Vec2{X: moveSpeed * -1, Y: 0.0}, true)
 	} else if action.input == MoveDown {
-		player.ApplyForceToCenter(box2d.B2Vec2{X: 0.0, Y: MoveSpeed * -1}, true)
+		player.ApplyForceToCenter(box2d.B2Vec2{X: 0.0, Y: moveSpeed * -1}, true)
 	} else if action.input == MoveRight{
-		player.ApplyForceToCenter(box2d.B2Vec2{X: MoveSpeed, Y: 0.0}, true)
+		player.ApplyForceToCenter(box2d.B2Vec2{X: moveSpeed, Y: 0.0}, true)
 	}
 	
 	for i := 0; i < 60; i++ {
-		world.Step(1.0 / 60.0, 6, 2)
+		e.World.Step(1.0 / 60.0, 6, 2)
 	}
 	return nil
+}
+
+func (e WorldProcessor) CreateCharacter() *box2d.B2Body{
+	bd := box2d.MakeB2BodyDef()
+	bd.Position.Set(2.0, 2.0)
+	bd.Type = box2d.B2BodyType.B2_dynamicBody
+	bd.FixedRotation = false	
+	bd.AllowSleep = false
+	
+	character := e.World.CreateBody(&bd)
+	shape := box2d.MakeB2PolygonShape()
+	shape.SetAsBox(0.20, 0.20)
+	fd := box2d.MakeB2FixtureDef()
+	fd.Shape = &shape
+	fd.Density = 1.0
+	character.CreateFixtureFromDef(&fd)
+	return character
+}
+
+func setupWorld() box2d.B2World{
+	gravity := box2d.MakeB2Vec2(0, 0)
+	return box2d.MakeB2World(gravity)
+}
+
+func NewProcessor() WorldProcessor {
+	world := setupWorld()
+	return WorldProcessor{World: &world}
 }
--- engine_test.go
+++ engine_test.go
@@ -21,38 +21,18 @@ func almostEqual(a, b float64) bool{
     return (d / math.Abs(b)) < tolerance
 }
 
-func setupWorld() (world box2d.B2World, character *box2d.B2Body) {
-	//Setup world
-	gravity := box2d.MakeB2Vec2(0, 0)
-	world = box2d.MakeB2World(gravity)
-	
-	//Setup Character
-	bd := box2d.MakeB2BodyDef()
-	bd.Position.Set(2.0, 2.0)
-	bd.Type = box2d.B2BodyType.B2_dynamicBody
-	bd.FixedRotation = false	
-	bd.AllowSleep = false
-	character = world.CreateBody(&bd)
-	shape := box2d.MakeB2PolygonShape()
-	shape.SetAsBox(0.20, 0.20)
-	fd := box2d.MakeB2FixtureDef()
-	fd.Shape = &shape
-	fd.Density = 1.0
-	character.CreateFixtureFromDef(&fd)
-	return
-}
-
 func TestActions(t *testing.T) {
 	t.Run("player goes UP", func(t *testing.T) {
 		expectedX := 2.0
 		expectedY := 3.0
-		world, character := setupWorld()
+		worldProcessor := NewProcessor()
+		character := worldProcessor.CreateCharacter()
 		playerName :=  "John"
 		action := Action{input: MoveUp, player: playerName}
 		playerStore := make(map[string] *box2d.B2Body)
 		playerStore[playerName] = character
 
-		ProcessWorld(&world, playerStore, action)
+		worldProcessor.Process(playerStore, action)
 
 		if !almostEqual(character.GetPosition().Y, expectedY) {
 			t.Errorf("player '%s' didn't go up correctly. Got %.1f expected %.1f", playerName, character.GetPosition().Y, expectedY)
@@ -66,13 +46,14 @@ func TestActions(t *testing.T) {
 	t.Run("player goes LEFT", func(t *testing.T) {
 		expectedX := 1.0
 		expectedY := 2.0
-		world, character := setupWorld()
+		worldProcessor := NewProcessor()
+		character := worldProcessor.CreateCharacter()
 		playerName :=  "John"
 		action := Action{input: MoveLeft, player: playerName}
 		playerStore := make(map[string] *box2d.B2Body)
 		playerStore[playerName] = character
 
-		ProcessWorld(&world, playerStore, action)
+		worldProcessor.Process(playerStore, action)
 
 		if !almostEqual(character.GetPosition().X, expectedX) {
 			t.Errorf("player '%s' didn't go left correctly. Got %.1f expected %.1f", playerName, character.GetPosition().X, expectedX)
@@ -86,13 +67,14 @@ func TestActions(t *testing.T) {
 	t.Run("player goes DOWN", func(t *testing.T) {
 		expectedX := 2.0
 		expectedY := 1.0
-		world, character := setupWorld()
+		worldProcessor := NewProcessor()
+		character := worldProcessor.CreateCharacter()
 		playerName :=  "John"
 		action := Action{input: MoveDown, player: playerName}
 		playerStore := make(map[string] *box2d.B2Body)
 		playerStore[playerName] = character
 
-		ProcessWorld(&world, playerStore, action)
+		worldProcessor.Process(playerStore, action)
 
 		if !almostEqual(character.GetPosition().Y, expectedY) {
 			t.Errorf("player '%s' didn't go down correctly. Got %.1f expected %.1f", playerName, character.GetPosition().Y, expectedY)
@@ -106,13 +88,14 @@ func TestActions(t *testing.T) {
 	t.Run("player goes RIGHT", func(t *testing.T) {
 		expectedX := 3.0
 		expectedY := 2.0
-		world, character := setupWorld()
+		worldProcessor := NewProcessor()
+		character := worldProcessor.CreateCharacter()
 		playerName :=  "John"
 		action := Action{input: MoveRight, player: playerName}
 		playerStore := make(map[string] *box2d.B2Body)
 		playerStore[playerName] = character
 
-		ProcessWorld(&world, playerStore, action)
+		worldProcessor.Process(playerStore, action)
 
 		if !almostEqual(character.GetPosition().X, expectedX) {
 			t.Errorf("player '%s' didn't go right correctly. Got %.1f expected %.1f", playerName, character.GetPosition().X, expectedX)
