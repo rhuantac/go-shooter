0d8ea23302ba334beb99d2f40d29561625cda6e3
Rhuan Chaves <rhuan.chaves@vollsolutions.com.br>
2023-12-27 21:00:43 -0300

    Added another manager

--- manager.go
+++ manager.go
@@ -1,5 +1,21 @@
 package main
 
-func CreateNewGame(processor Processor){
-	processor.CreateCharacter()
+import "github.com/ByteArena/box2d"
+
+type PlayerStore map[string]*box2d.B2Body
+type GameManager struct {
+	gameProcessor Processor
+	playerStore PlayerStore
+}
+
+func CreateNewGame(processor Processor) GameManager{
+	store := make(PlayerStore, 0)
+	store["John"] = processor.CreateCharacter()
+	return GameManager{gameProcessor: processor, playerStore: store}
+}
+
+func gameLoop(processor Processor, players PlayerStore) {
+	for i := 0; i < 60; i++ {
+		processor.Process(players, Action{input: MoveDown, player: "John"})
+	}
 }
--- manager_test.go
+++ manager_test.go
@@ -8,9 +8,11 @@ import (
 
 type StubWorldProcessor struct {
 	createCharacterCalls int
+	processCalls int
 }
 
 func (w *StubWorldProcessor) Process(playerStore map[string]*box2d.B2Body, action Action) error{
+	w.processCalls += 1
 	return nil	
 }
 
@@ -19,11 +21,21 @@ func (w *StubWorldProcessor) CreateCharacter() *box2d.B2Body{
 	return nil
 }
 func TestGameManager(t *testing.T) {
-	t.Run("Test that one character is created", func(t *testing.T) {
+	t.Run("Test that one character is created on new game", func(t *testing.T) {
 		processor := StubWorldProcessor{}
 		CreateNewGame(&processor)
 		if processor.createCharacterCalls != 1 {
 			t.Errorf("got %d calls on CreateCharacter want %d", processor.createCharacterCalls, 1)
+		}
+	})
+
+	t.Run("Test that World is processed 60 ticks per call", func (t *testing.T) {
+		processor := StubWorldProcessor{}
+		playerStore := make(PlayerStore)
+		CreateNewGame(&processor)
+		gameLoop(&processor, playerStore)
+		if (processor.processCalls != 60) {
+			t.Errorf("got %d calls on gameLoop want %d", processor.processCalls, 60)
 		}
 	})
 }
